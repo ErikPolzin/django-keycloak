14408a56a9d80aee62f71ba58c559f0fabe01eac
Erik Polzin <eriktpol@gmail.com>
2024-06-04 09:23:11 +0200

    Add example project

---
+++ examples/resource_provider_api/resource_provider_api/views.py
@@ -0,0 +1,19 @@
+import logging
+
+from django.http.response import JsonResponse
+
+logger = logging.getLogger(__name__)
+
+
+def api_end_point(request):
+    return JsonResponse({'foo': 'bar'})
+
+
+def authenticated_end_point(request):
+    return JsonResponse({
+        'user': {
+            'username': request.user.username,
+            'first_name': request.user.first_name,
+            'last_name': request.user.last_name
+        }
+    })
