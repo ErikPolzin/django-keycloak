b5f53068130faed6f55d99fd43a34ebd686cad03
Erik Polzin <eriktpol@gmail.com>
2024-06-13 22:02:31 +0200

    Change add_client script

--- src/django_keycloak/management/commands/add_client.py
+++ src/django_keycloak/management/commands/add_client.py
@@ -8,17 +8,15 @@ from django_keycloak.models import Realm, Client
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('realm_name', type=str)
+        parser.add_argument('client_id', type=str)
 
-    def handle(self, realm_name: str, **kwargs):
-        try:
-            realm = Realm.objects.get(name=realm_name)
-        except Realm.DoesNotExist:
-            self.stderr.write(f"No realm called '{realm_name}'")
+    def handle(self, client_id: str, **kwargs):
+        realm = Realm.objects.first()
+        if realm is None:
+            self.stderr.write("No realm found")
             return
-        client_id = input("Client ID: ")
         client_secret = getpass.getpass("Client Secret [optional]: ") or None
         Client.objects.create(realm=realm,
                               client_id=client_id,
                               secret=client_secret)
-        self.stdout.write(f"Created client for realm '{realm_name}'")
+        self.stdout.write(f"Created client for realm '{client_id}'")
